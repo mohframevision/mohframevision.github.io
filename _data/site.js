1555d93e3587e43c72d74f8d4c33d6cc1855a767
mohframevision <134697684+mohframevision@users.noreply.github.com>
2026-07-29 09:13:49 +0300

    2026

---
+++ _data/site.js
@@ -0,0 +1,14 @@
+module.exports = {
+  url: "https://mohframevision.github.io",
+  nav: [
+    { href: "index.html", label: "الرئيسية" },
+    { href: "about.html", label: "عنّي" },
+    { href: "projects.html", label: "المشاريع" },
+    { href: "skills.html", label: "المهارات" },
+    { href: "resources.html", label: "موارد مفيدة" },
+    { href: "naqsh.html", label: "نقش ستوديو" },
+    { href: "services.html", label: "الخدمات" },
+    { href: "contact.html", label: "تواصل" }
+  ],
+  defaultOgImage: "https://mohframevision.github.io/images/og-image.png"
+};
